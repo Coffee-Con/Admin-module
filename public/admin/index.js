d67fe0e15a0724ecda0c499ac81d93dd9a03318f
EternalIndex <156399428+EternalIndex@users.noreply.github.com>
2024-10-08 21:39:33 +0800

    Group

--- public/admin/index.js
+++ public/admin/index.js
@@ -6,12 +6,36 @@ const formEvent = form.addEventListener("submit", (event) => {
   sendMail(mail);
 });
 
+/*
 const sendMail = (mail) => {
   fetch("/send", {
     method: "post",
     body: mail,
   }).then((response) => {
     return response.text(); // 更改为返回文本
+  }).then((data) => {
+    document.getElementById("result-message").innerHTML = data;
+  }).catch((error) => {
+    document.getElementById("result-message").innerHTML = "An error occurred: " + error;
+  });
+};
+*/
+const sendMail = (mail) => {
+  // 获取所有的 Name 和 Email 输入字段
+  const names = document.getElementsByName('name[]');
+  const emails = document.getElementsByName('email[]');
+  
+  // 将每个收件人添加到表单数据中
+  for (let i = 0; i < names.length; i++) {
+    mail.append(`name[${i}]`, names[i].value);
+    mail.append(`email[${i}]`, emails[i].value);
+  }
+
+  fetch("/send", {
+    method: "post",
+    body: mail,
+  }).then((response) => {
+    return response.text();
   }).then((data) => {
     document.getElementById("result-message").innerHTML = data;
   }).catch((error) => {
